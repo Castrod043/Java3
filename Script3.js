c80d01b2851d45470b228f94a8efbfadbb658235
Castrod043 <64606385+Castrod043@users.noreply.github.com>
2021-09-09 22:28:10 +0200

    Update Script3.js

--- Script3.js
+++ Script3.js
@@ -1,3 +1,61 @@
+console.log('Java script opdracht2')
+
+//Deel 1 
+
+const Age = 17; /* Voer hier je leeftijd in */
+
+if (Age >= 18) {
+
+    console.log('Welkom je mag naar binnen')
+}
+
+if (Age < 18) {
+
+    console.log('Helaas je bent te jong')
+}
+
+
+
+// Deel 2 
+
+const isFemale = false; /* true=vrouw false=anders*/
+
+if (isFemale == true) {
+
+    console.log('wees welkom')
+
+}
+
+if (isFemale == false) {
+
+    console.log('wegwezen')
+}
+
+/*
+= 
+“Is”
+“Ik verander deze variable naar”
+
+== 
+“Is gelijk aan”
+“Ik check of x gelijk is aan y*/
+
+
+//Deel 3 
+
+const driverStatus = "Bob";  /*Andere  input dan Bob = Taxi*/
+
+if (driverStatus == "Bert") {
+
+    console.log('Drive safe, sweet dreams')
+
+} else {
+
+    console.log('Wij bellen een een taxi')
+}
+
+
+
 console.log('Comparison && Logical Operators')
 
 //Deel 1 
